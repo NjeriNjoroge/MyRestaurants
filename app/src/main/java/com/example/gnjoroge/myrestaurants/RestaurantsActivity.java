ac072b8c66308f0135cefe7f86fcff0801d02dbb
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-09-06 10:56:11 +0300

    introduced butterknife to the application and log messages with logcat

--- app/src/main/java/com/example/gnjoroge/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/gnjoroge/myrestaurants/RestaurantsActivity.java
@@ -3,6 +3,7 @@ package com.example.gnjoroge.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,9 +11,13 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class RestaurantsActivity extends AppCompatActivity {
-    private TextView mLocationTextView;
-    private ListView mListView;
+    @Bind(R.id.locationTextView) TextView mLocationTextView;
+    @Bind(R.id.listView) ListView mListView;
+    public static final String TAG = RestaurantsActivity.class.getSimpleName();
 
     private String[] restaurants = new String[] {"Java", "ArtCaffe",
             "Cafe Deli", "Kaldis", "Cold Stone Creamerys", "Galitos",
@@ -24,8 +29,7 @@ public class RestaurantsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
-        mListView = (ListView) findViewById(R.id.listView); //initializing the list view
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        ButterKnife.bind(this);
 
         //implementing an arrayAdapter
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
@@ -37,12 +41,14 @@ public class RestaurantsActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String restaurant = ((TextView)view).getText().toString();
                 Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_SHORT).show();
+                Log.v(TAG, "In the onItemClickListener!");
             }
         });
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurats near:" + location);
+        Log.d(TAG, "Inthe onCreate method!");
     }
 
 }
